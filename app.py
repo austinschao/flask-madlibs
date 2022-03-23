90feac7e315b760142f5f3cbe17ab584a266e328
Austin Chao <austinschao@gmail.com>
2022-03-23 16:36:55 -0700

    Created form for users to submit questions and return a story

--- app.py
+++ app.py
@@ -7,3 +7,19 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get("/questions")
+def display_questions():
+    """ Display each word from the prompt """
+
+    return render_template("questions.html", prompts = silly_story.prompts)
+
+
+@app.get("/story")
+def retrieve_answers():
+    """ Retrieve answers from form """
+
+    story = silly_story.generate(request.args)
+    return render_template("story.html", story = story)
+
